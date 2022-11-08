23c936bf6e8700be78e449baff067c5109879965
jdlms <97018563+jdlms@users.noreply.github.com>
2022-11-08 13:40:50 +0100

    like button refactor

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -2,23 +2,19 @@ import { useState } from 'react';
 
 export function LikeButton() {
   const [likeState, setLikeState] = useState(0);
-  const [colorState, setColorState] = useState(0);
+  const [colorIndex, setColorIndex] = useState(0);
 
-  const color = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
+  const colorArr = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
 
   const handleClick = () => {
     setLikeState((old) => old + 1);
-    setColorState((old) => {
-      if (old > 4) {
-        return old * 0;
-      } else return old + 1;
-    });
+    setColorIndex((old) => (old > 4 ? old * 0 : old + 1));
   };
 
   return (
     <div>
       <button
-        style={{ height: 35, width: 60, backgroundColor: color[colorState] }}
+        style={{ height: 35, width: 60, backgroundColor: colorArr[colorIndex] }}
         value={likeState}
         onClick={handleClick}
       >
